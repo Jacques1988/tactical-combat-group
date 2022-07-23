b4de1317e07a9a4d42cafeae21434a429b06b393
Jacques <jacquesvanluyck@gmail.com>
2022-07-23 22:48:47 +0200

    update header background

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,11 +9,20 @@
     <link href="./dist/main.css" rel="stylesheet">
     <title>Airsoft auf Rügen</title>
 </head>
+
 <body>
-    <header>
-        <a href="index.php">
-        <img class="logo header__logo" src="./img/Logo_transparent.png" alt="Captain's Pride tactical combat group">
-        </a>
+    <header class="header">
+        <nav class="header__navigation">
+            <div class="header__navigation__menu">
+                <span class="header__navigation__menu__bar"></span>
+                <span class="header__navigation__menu__bar"></span>
+            </div>
+            <a class="header__navigation__link" href="index.php">
+                <img class="logo header__navigation__link__logo" src="./img/Logo_transparent.png" alt="Captain's Pride tactical combat group">
+            </a>
+            <button class="button-primary header__navigation__button">Kontakt</button>
+        </nav>
     </header>
 </body>
+
 </html>
