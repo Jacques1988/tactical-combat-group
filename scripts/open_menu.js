b7f00cee732f146bc619f3f9b850b2c3612ff339
Jacques <jacquesvanluyck@gmail.com>
2022-07-28 04:53:00 +0200

    update menu

--- scripts/open_menu.js
+++ scripts/open_menu.js
@@ -1,9 +1,9 @@
 function open_menu(){
     document.getElementById('menu').style.opacity = '1';
-    document.getElementById('menu').style.zIndex = '10';
+    document.getElementById('menu').style.zIndex = '2';
 }
 
 function close_menu(){
     document.getElementById('menu').style.opacity = '0';
-    document.getElementById('menu').style.zIndex = '-10';
+    document.getElementById('menu').style.zIndex = '-2';
 }
